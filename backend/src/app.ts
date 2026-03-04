b33ae8276a192028aa25170f0e0e8e032a10fd9d
kiyan javanshiri <kiyanjavanshiri@gmail.com>
2026-03-04 20:39:32 +0200

    feat: add function to connect to database

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,9 +1,23 @@
 import express from "express";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+
+dotenv.config();
 
 const app = express();
 
-const PORT = 3000;
+const PORT = process.env.APP_URL || 3000;
 
-app.listen(PORT, () => {
-  console.log(`server is running at port ${PORT}`);
-});
+const start = async () => {
+  try {
+    await mongoose.connect(process.env.DB_URL!);
+    console.log("database was connected successfully")
+    app.listen(PORT, () => {
+      console.log(`server is running at port ${PORT}`);
+    });
+  } catch (ex) {
+    console.error(ex);
+  }
+};
+
+start();
