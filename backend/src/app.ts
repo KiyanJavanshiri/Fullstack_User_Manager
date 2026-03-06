ee14e960d457ac3278b9d4011cadae6cdfd22f49
kiyan javanshiri <kiyanjavanshiri@gmail.com>
2026-03-06 10:42:03 +0200

    feat: add cors

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,4 +1,5 @@
 import express from "express";
+import cors from "cors";
 import mongoose from "mongoose";
 import dotenv from "dotenv";
 import { errorHandler } from "./utils/errorHandler";
@@ -8,6 +9,7 @@ dotenv.config();
 
 const app = express();
 
+app.use(cors());
 app.use(express.json());
 app.use("/api/users", usersRouter);
 app.use(errorHandler);
